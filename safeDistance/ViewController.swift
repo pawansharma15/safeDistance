55a36f6f8307b4d74171e1b967fbb6cbc76c0aba
Pawan Sharma <pawan4u_sharma@yahoo.co.in>
2020-05-25 11:11:55 +0100

    Initial Commit

---
+++ safeDistance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  safeDistance
+//
+//  Created by Swapna Maskar on 25/05/2020.
+//  Copyright © 2020 pratham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
